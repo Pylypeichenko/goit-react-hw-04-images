48460dd5dd51b30f6af6f6d4194a122de843501a
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-10-28 23:15:30 +0300

    modal hook

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,39 +1,66 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal_root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscapeKeydown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscapeKeydown);
-  }
-
-  handleEscapeKeydown = e => {
+const Modal = ({ children, onModalClose }) => {
+  const handleEscapeKeydown = e => {
     if (e.code === 'Escape') {
-      this.props.onModalClose();
+      onModalClose();
     }
   };
 
-  handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onModalClose();
+      onModalClose();
     }
   };
 
-  render() {
-    const { children } = this.props;
-    return createPortal(
-      <div className={css.Modal__backdrop} onClick={this.handleBackdropClick}>
-        <div className={css.Modal__window}>{children}</div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  useEffect(() => {
+    window.addEventListener('keydown', handleEscapeKeydown);
+
+    return window.removeEventListener('keydown', handleEscapeKeydown);
+  }, []);
+
+  return createPortal(
+    <div className={css.Modal__backdrop} onClick={handleBackdropClick}>
+      <div className={css.Modal__window}>{children}</div>
+    </div>,
+    modalRoot
+  );
+};
+
+// class OldModal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleEscapeKeydown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleEscapeKeydown);
+//   }
+
+//   handleEscapeKeydown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onModalClose();
+//     }
+//   };
+
+//   handleBackdropClick = e => {
+//     if (e.target === e.currentTarget) {
+//       this.props.onModalClose();
+//     }
+//   };
+
+//   render() {
+//     const { children } = this.props;
+//     return createPortal(
+//       <div className={css.Modal__backdrop} onClick={this.handleBackdropClick}>
+//         <div className={css.Modal__window}>{children}</div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
 
 export default Modal;
