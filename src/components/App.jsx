8fe319f0c42b2f09f9fc3cc2e0b24974ad2df098
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-10-28 23:41:44 +0300

    Done (I think)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,35 +9,22 @@ import Button from './Button/Button';
 import Spinner from './Spinner/Spinner';
 import Modal from './Modal/Modal';
 import fetchData from '../api-services/fetchData';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    images: [],
-    page: 1,
-    perPage: 12,
-    showModal: false,
-    selectedImage: '',
-    status: 'idle',
-    error: null,
-    totalQueryHits: 0,
-  };
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(12);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
+  const [status, setStatus] = useState('idle');
+  const [error, setError] = useState(null);
+  const [totalQueryHits, setTotalQueryHits] = useState(0);
 
-  componentDidMount() {
-    console.log('componentDidMount');
-    this.createGallery();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      console.log('componentDidUpdate');
-      this.createGallery();
-    }
-  }
-
-  createGallery = () => {
-    this.setState({ status: 'pending' });
-    const { searchQuery, perPage, page } = this.state;
+  const createGallery = () => {
+    setStatus('pending');
 
     return fetchData(searchQuery, perPage, page)
       .then(data => {
@@ -49,7 +36,7 @@ export class App extends Component {
           Notify.success(`We found ${data.totalHits} images on your request`);
         }
 
-        this.setState({ totalQueryHits: data.totalHits });
+        setTotalQueryHits(data.totalHits);
 
         const imagesArray = data.hits.map(obj => {
           const newObj = {
@@ -62,77 +49,197 @@ export class App extends Component {
           return newObj;
         });
 
-        return this.setState(({ images }) => ({
-          images: [...images, ...imagesArray],
-          status: 'resolved',
-        }));
+        setStatus('resolved');
+        return setImages([...images, ...imagesArray]);
       })
       .catch(error => {
         Notify.error('Something went wrong. Please try again.');
-        this.setState({ error, status: 'rejected' });
+        setError(error);
+        setStatus('rejected');
       })
-      .finally(this.pageIncrement());
+      .finally(pageIncrement());
   };
 
-  pageReset = () => {
-    this.setState({ page: 1 });
+  const pageIncrement = () => {
+    setPage(state => state + 1);
   };
 
-  pageIncrement = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
-  getSearchQuery = data => {
+  const getSearchQuery = data => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
 
-    this.setState({ searchQuery: data, images: [] });
-    this.pageReset();
+    setImages([]);
+    setSearchQuery(data);
+    setPage(1);
   };
 
-  modalOpen = () => {
-    if (!this.state.showModal) {
-      this.setState({ showModal: true });
+  const modalOpen = () => {
+    if (!showModal) {
+      setShowModal(true);
     }
   };
 
-  modalClose = () => {
-    if (this.state.showModal) {
-      this.setState({ showModal: false });
+  const modalClose = () => {
+    if (showModal) {
+      setShowModal(false);
     }
   };
 
-  getImageData = data => {
-    this.setState({ selectedImage: data });
-    this.modalOpen();
+  const getImageData = data => {
+    setSelectedImage(data);
+    modalOpen();
   };
 
-  render() {
-    const { images, status, showModal, selectedImage, error, totalQueryHits } =
-      this.state;
+  useEffect(() => {
+    createGallery();
+  }, [searchQuery]);
 
-    return (
-      <div className={css.App}>
-        {showModal && (
-          <Modal onModalClose={this.modalClose}>
-            <img src={selectedImage.largeImageURL} alt={selectedImage.tags} />
-          </Modal>
-        )}
+  return (
+    <div className={css.App}>
+      {showModal && (
+        <Modal onModalClose={modalClose}>
+          <img src={selectedImage.largeImageURL} alt={selectedImage.tags} />
+        </Modal>
+      )}
 
-        <Searchbar onFormSubmit={this.getSearchQuery} />
+      <Searchbar onFormSubmit={getSearchQuery} />
 
-        {status === 'rejected' && <p>{error.message}</p>}
+      {status === 'rejected' && <p>{error.message}</p>}
 
-        <ImageGallery images={images} getImageObj={this.getImageData} />
+      <ImageGallery images={images} getImageObj={getImageData} />
 
-        {status === 'pending' && <Spinner />}
+      {status === 'pending' && <Spinner />}
 
-        {images.length < totalQueryHits && (
-          <Button handleClick={this.createGallery} />
-        )}
-      </div>
-    );
-  }
-}
+      {images.length < totalQueryHits && <Button handleClick={createGallery} />}
+    </div>
+  );
+};
+
+// export class OldApp extends Component {
+//   state = {
+//     searchQuery: '',
+//     images: [],
+//     page: 1,
+//     perPage: 12,
+//     showModal: false,
+//     selectedImage: '',
+//     status: 'idle',
+//     error: null,
+//     totalQueryHits: 0,
+//   };
+
+//   componentDidMount() {
+//     console.log('componentDidMount');
+//     this.createGallery();
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.searchQuery !== this.state.searchQuery) {
+//       console.log('componentDidUpdate');
+//       this.createGallery();
+//     }
+//   }
+
+//   createGallery = () => {
+//     this.setState({ status: 'pending' });
+//     const { searchQuery, perPage, page } = this.state;
+
+//     return fetchData(searchQuery, perPage, page)
+//       .then(data => {
+//         if (data.totalHits === 0) {
+//           Notify.warning(`We couldn't find anything.
+//             Try another word for search`);
+//         }
+//         if (data.totalHits !== 0) {
+//           Notify.success(`We found ${data.totalHits} images on your request`);
+//         }
+
+//         this.setState({ totalQueryHits: data.totalHits });
+
+//         const imagesArray = data.hits.map(obj => {
+//           const newObj = {
+//             id: obj.id,
+//             webformatURL: obj.webformatURL,
+//             tags: obj.tags,
+//             largeImageURL: obj.largeImageURL,
+//           };
+
+//           return newObj;
+//         });
+
+//         return this.setState(({ images }) => ({
+//           images: [...images, ...imagesArray],
+//           status: 'resolved',
+//         }));
+//       })
+//       .catch(error => {
+//         Notify.error('Something went wrong. Please try again.');
+//         this.setState({ error, status: 'rejected' });
+//       })
+//       .finally(this.pageIncrement());
+//   };
+
+//   pageReset = () => {
+//     this.setState({ page: 1 });
+//   };
+
+//   pageIncrement = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1 }));
+//   };
+
+//   getSearchQuery = data => {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: 'smooth',
+//     });
+
+//     this.setState({ searchQuery: data, images: [] });
+//     this.pageReset();
+//   };
+
+//   modalOpen = () => {
+//     if (!this.state.showModal) {
+//       this.setState({ showModal: true });
+//     }
+//   };
+
+//   modalClose = () => {
+//     if (this.state.showModal) {
+//       this.setState({ showModal: false });
+//     }
+//   };
+
+//   getImageData = data => {
+//     this.setState({ selectedImage: data });
+//     this.modalOpen();
+//   };
+
+//   render() {
+//     const { images, status, showModal, selectedImage, error, totalQueryHits } =
+//       this.state;
+
+//     return (
+//       <div className={css.App}>
+//         {showModal && (
+//           <Modal onModalClose={this.modalClose}>
+//             <img src={selectedImage.largeImageURL} alt={selectedImage.tags} />
+//           </Modal>
+//         )}
+
+//         <Searchbar onFormSubmit={this.getSearchQuery} />
+
+//         {status === 'rejected' && <p>{error.message}</p>}
+
+//         <ImageGallery images={images} getImageObj={this.getImageData} />
+
+//         {status === 'pending' && <Spinner />}
+
+//         {images.length < totalQueryHits && (
+//           <Button handleClick={this.createGallery} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,44 +1,79 @@
-import { Component } from 'react';
+import { useState } from 'react';
+
 import css from './Searchbar-styles.module.css';
 
-class Searchbar extends Component {
-  state = {
-    inputValue: '',
+const Searchbar = ({ onFormSubmit }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = event => {
+    setInputValue(event.currentTarget.value);
   };
 
-  handleInputChange = event => {
-    this.setState({
-      inputValue: event.currentTarget.value,
-    });
-  };
-
-  handleFormSubmit = event => {
+  const handleFormSubmit = event => {
     event.preventDefault();
 
-    this.props.onFormSubmit(this.state.inputValue.toLowerCase().trim());
+    onFormSubmit(inputValue.toLowerCase().trim());
   };
 
-  render() {
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.handleFormSubmit}>
-          <button type="submit" className={css.SearchForm__button}>
-            <span className={css.SearchForm__button_label}>Search</span>
-          </button>
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={handleFormSubmit}>
+        <button type="submit" className={css.SearchForm__button}>
+          <span className={css.SearchForm__button_label}>Search</span>
+        </button>
 
-          <input
-            className={css.SearchForm__input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.inputValue}
-            onChange={this.handleInputChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={css.SearchForm__input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </form>
+    </header>
+  );
+};
 
 export default Searchbar;
+
+// class OldSearchbar extends Component {
+//   state = {
+//     inputValue: '',
+//   };
+
+//   handleInputChange = event => {
+//     this.setState({
+//       inputValue: event.currentTarget.value,
+//     });
+//   };
+
+//   handleFormSubmit = event => {
+//     event.preventDefault();
+
+//     this.props.onFormSubmit(this.state.inputValue.toLowerCase().trim());
+//   };
+
+//   render() {
+//     return (
+//       <header className={css.Searchbar}>
+//         <form className={css.SearchForm} onSubmit={this.handleFormSubmit}>
+//           <button type="submit" className={css.SearchForm__button}>
+//             <span className={css.SearchForm__button_label}>Search</span>
+//           </button>
+
+//           <input
+//             className={css.SearchForm__input}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={this.state.inputValue}
+//             onChange={this.handleInputChange}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
